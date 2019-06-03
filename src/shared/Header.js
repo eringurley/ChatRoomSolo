18b26ac5013f0d63dc0bb38c8e7e69f5c4045437
Erin Gurley <erinmgurley@gmail.com>
2019-06-03 15:41:37 -0700

    rendered profile component

--- src/shared/Header.js
+++ src/shared/Header.js
@@ -1,6 +1,17 @@
 import Component from '../Component.js';
+import Profile from './Profile.js';
+import { auth } from '../services/firebase.js';
 
 class Header extends Component {
+    render() {
+        const dom = this.rendorDOM();
+        const profile = new Profile();
+        dom.appendChile(profile.render());
+
+        auth.onAuthStateChanged(user => {
+            profile.update({ user });
+        });
+    }
     renderTemplate() {
         // const title = this.props.title || document.title;
 
