42acd0b5060283724f0a5c58eac8d1b4cb888408
Erin Gurley <erinmgurley@gmail.com>
2019-06-03 16:00:36 -0700

    completed the rest of the profile tests

--- test/Profile.test.js
+++ test/Profile.test.js
@@ -7,7 +7,7 @@ test('renders with full user props', assert => {
 
      //arrange
     const user = {
-        displayName: 'Watcher',
+        displayName: 'Chatter',
         photoURL: 'https://lh5.googleusercontent.com/-EnRNBAYvURk/AAAAAAAAAAI/AAAAAAAAAK8/cMJNFAVX5SE/photo.jpg'
     };
     const profile = new Profile({ user });
@@ -26,39 +26,39 @@ test('renders with full user props', assert => {
     assert.htmlEqual(rendered, expected);
 });
 
-// test('renders with default avatar when no photoURL', assert => {
-//     // arrange
-//     const user = {
-//         displayName: 'Marty Nelson',
-//         photoURL: null
-//     };
-//     const profile = new Profile({ user });
-//     const expected = /*html*/`
-//         <div class="profile">
-//             <img src="./assets/default-avatar.png">
-//             <span>Marty Nelson</span>
-//             <button>Sign Out</button>
-//         </div>
-//     `;
+test('renders with default avatar when no photoURL', assert => {
+    // arrange
+    const user = {
+        displayName: 'Marty Nelson',
+        photoURL: null
+    };
+    const profile = new Profile({ user });
+    const expected = /*html*/`
+        <div class="profile">
+            <img src="./assets/default-avatar.png">
+            <span>Marty Nelson</span>
+            <button>Sign Out</button>
+        </div>
+    `;
 
-//      // act
-//     const rendered = profile.renderTemplate();
+     // act
+    const rendered = profile.renderTemplate();
 
-//      // assert
-//     assert.htmlEqual(rendered, expected);
-// });
+     // assert
+    assert.htmlEqual(rendered, expected);
+});
 
-// test('renders empty div when no user', assert => {
-//     // arrange
-//     const user = null;
-//     const profile = new Profile({ user });
-//     const expected = /*html*/`
-//         <div></div>
-//     `;
+test('renders empty div when no user', assert => {
+    // arrange
+    const user = null;
+    const profile = new Profile({ user });
+    const expected = /*html*/`
+        <div></div>
+    `;
 
-//      // act
-//     const rendered = profile.renderTemplate();
+     // act
+    const rendered = profile.renderTemplate();
 
-//      // assert
-//     assert.htmlEqual(rendered, expected);
-// }); 
+     // assert
+    assert.htmlEqual(rendered, expected);
+}); 
