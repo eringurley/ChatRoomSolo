f652e4a836df8bfa901416bdf175b4ba8395f5cd
Erin Gurley <erinmgurley@gmail.com>
2019-06-03 15:57:55 -0700

    return dom to fix console errors and populate page

--- src/shared/Header.js
+++ src/shared/Header.js
@@ -3,15 +3,18 @@ import Profile from './Profile.js';
 import { auth } from '../services/firebase.js';
 
 class Header extends Component {
+
     render() {
-        const dom = this.rendorDOM();
+        const dom = this.renderDOM();
         const profile = new Profile();
         dom.appendChild(profile.render());
 
         auth.onAuthStateChanged(user => {
             profile.update({ user });
         });
-    }
+
+        return dom;
+    }   
     renderTemplate() {
         // const title = this.props.title || document.title;
 
