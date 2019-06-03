4ac64d64ee5d6586ab356e8645ded732c52d6fe7
Erin Gurley <erinmgurley@gmail.com>
2019-06-03 15:58:09 -0700

    corrected firebase files

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,13 +1,13 @@
 const config = {
-    apiKey: 'AIzaSyCoEHEjb87b2VF8p9JVPFZ2zrNOKL5C_MY',
-    authDomain: 'watchy-things.firebaseapp.com',
-    databaseURL: 'https://watchy-things.firebaseio.com',
-    projectId: 'watchy-things',
-    storageBucket: '',
-    messagingSenderId: '127456624729',
-    appId: '1:127456624729:web:595d7370dda355d9'
+    apiKey: 'AIzaSyAJiIKqmSES8zv6ypDlZWCJaAW_OyrIU9w',
+    authDomain: 'chat-room-3c433.firebaseapp.com',
+    databaseURL: 'https://chat-room-3c433.firebaseio.com',
+    projectId: 'chat-room-3c433',
+    storageBucket: 'chat-room-3c433.appspot.com',
+    messagingSenderId: '757552031065',
+    appId: '1:757552031065:web:a3bce3dcc4a9857f'
 };
 
 firebase.initializeApp(config);
 
-export const auth = firebase.auth(); 
+export const auth = firebase.auth();
