b637cad0a0e0b9921d9817fe2f42e40c878d962b
Erin Gurley <erinmgurley@gmail.com>
2019-06-05 13:50:46 -0700

    added chat.html and was able to get data to read from firebase to the console

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import Component from './Component.js';
 import Header from './shared/Header.js';
+import ChatList from '../src/chat/ChatList.js';
+import { chatRef } from './services/firebase.js';
 
 class App extends Component {
 
@@ -8,8 +10,22 @@ class App extends Component {
 
         const header = new Header({ title: 'Welcome to Chats' });
         const headerDOM = header.render();
+        
         const main = dom.querySelector('main');
         dom.insertBefore(headerDOM, main);
+
+        const chatList = new ChatList();
+        const chatListDOM = chatList.renderDOM();
+        main.appendChild(chatListDOM);
+
+        //fetch
+        chatRef
+            .on('value', snapshot => {
+                const value = snapshot.val();
+                const chat = value ? Object.values(value) : [];
+                chatList.update({ chat });
+            });
+
 
         return dom;
     }
--- src/chat/ChatList.js
+++ src/chat/ChatList.js
@@ -1,19 +1,29 @@
 import Component from '../Component.js';
 import ChatItem from './ChatItem.js';
+// import { auth, chatRef } from '../services/firebase.js';
+
 
 class ChatList extends Component {
     render() {
         const list = this.renderDOM();
-
+        console.log(this.props)
         this.props.chat.forEach(chat => {
             const chatItem = new ChatItem({ chat });
             list.appendChild(chatItem.render());
         });
+
+        // chatRef
+        //     .child(auth.currentUser.uid)
+        //     .on('value', snapshot => {
+        //         const value = snapshot.val();
+        //         const chat = value ? Object.values(value) : [];
+        //         ChatList.update({ chat });
+        //     });
     }
 
     renderTemplate() {
         return /*html*/`
-            <ul class="chat-list"></ul>
+            <ul class="chat-list">hello</ul>
         `;
     }
 }
