1699e548025dc8c76d9a86c1a3b580c6c65278d4
Erin Gurley <erinmgurley@gmail.com>
2019-06-04 20:27:35 -0700

    seeing if this will pass travis in my tests.js

--- test/tests.js
+++ test/tests.js
@@ -1,7 +1,3 @@
-import { app } from '../src/services/firebase.js'; 
 import './html-equal.js';
+// import utilities and custom assertions
 import './Profile.test.js';
-
-QUnit.done(() => {
-    app.delete();
-}); 
