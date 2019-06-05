9e97ed11549d9fc236b1376165bcc464199186f9
Erin Gurley <erinmgurley@gmail.com>
2019-06-05 12:43:49 -0700

    added firebase export

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -13,4 +13,4 @@ export const app = firebase.initializeApp(config);
 export const auth = firebase.auth();
 export const db = firebase.database();
 
-export const userFavoritesRef = db.ref('userFavorites');
+export const chatRef = db.ref('chat');
