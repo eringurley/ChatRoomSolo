5dad609bfa197547e2bf69f103a89d7cbe9ec5e1
Erin Gurley <erinmgurley@gmail.com>
2019-06-06 12:36:02 -0700

    corrected chatItem tests and item

--- test/tests.js
+++ test/tests.js
@@ -1,8 +1,3 @@
 import './html-equal.js';
-import { app } from '../src/services/firebase.js'; 
 import './Profile.test.js';
 import './chatItem.test.js';
-
-QUnit.done(() => {
-    app.delete();
-}); 
