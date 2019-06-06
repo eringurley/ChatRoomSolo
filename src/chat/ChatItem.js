320d01e2bbc2a824680904a2400d63fd5ea2003f
Erin Gurley <erinmgurley@gmail.com>
2019-06-06 12:38:19 -0700

    created a remove button for chats

--- src/chat/ChatItem.js
+++ src/chat/ChatItem.js
@@ -1,17 +1,31 @@
 import Component from '../Component.js';
+import { auth, chatRef } from '../services/firebase.js';
+
 
 class ChatItem extends Component {
     render() {
         const dom = this.renderDOM();
+        const chat = this.props.chat;
+
+        const button = dom.querySelector('button');
+        if(button) {
+            button.addEventListener('click', () => {
+                chatRef.child(chat.key).remove();
+            });
+        }
 
         return dom;
     }
 
     renderTemplate() {
         const chat = this.props.chat;
+        const isOwner = auth.currentUser === chat.owner;
+        const button = isOwner ? '<button>ⓧ</button>' : '';
+
         return /*html*/`
-            <li class="chat-item">
-                <h2>Chat List</h2>
+                <li>
+                    <a href="./chat.html?key=${chat.key}">${chat.title}</a>
+                    ${button}
                 </li>
         `;
     }
