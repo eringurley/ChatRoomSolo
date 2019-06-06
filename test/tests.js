678995458d8f9d8985fb3c0faec0c0cc63d60598
Erin Gurley <erinmgurley@gmail.com>
2019-06-06 14:47:39 -0700

    updated tests

--- test/tests.js
+++ test/tests.js
@@ -1,3 +1,8 @@
+import { app } from '../src/services/firebase.js';
 import './html-equal.js';
 import './Profile.test.js';
 import './chatItem.test.js';
+
+QUnit.done(() => {
+    app.delete();
+});
